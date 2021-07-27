964393d4b79f1342a2a36b453c1ee0aeb65643fb
ophir.h <ophir.h@quali.com>
2021-07-28 00:23:11 +0300

    exempt failed urls

--- download.py
+++ download.py
@@ -18,23 +18,26 @@ def download(url):
     tempFile = VIDEOS_FOLDER + '/' +'video' + '.mp4'
     ydl_opts = {
     'outtmpl': tempFile
-}
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
             songInfo = ydl.extract_info(url, download=True)
-            title = songInfo['title']
-            title = title.replace("!@#$%^&*()[]{};:,./<>?\/|`~-=_+", " ")
-            newFileName = VIDEOS_FOLDER + '/' + title + '.mp4'
-            try:
-                os.replace(tempFile, newFileName)
-            except:
+        except:
+            print('could not process link: ' + url)
+        title = songInfo['title']
+        title = title.replace("!@#$%^&*()[]{};:,./<>?\/|`~-=_+", " ")
+        newFileName = VIDEOS_FOLDER + '/' + title + '.mp4'
+        try:
+            os.replace(tempFile, newFileName)
+        except:
                 unnamedPath = VIDEOS_FOLDER + '/' + UNNAMED_FOLDER
                 newFileName = unnamedPath + '/' + 'SONG-' + str(uuid.uuid4()) + '.mp4'
                 if not os.path.exists(unnamedPath):
                     os.makedirs(unnamedPath)
                 os.replace(tempFile, newFileName)
-            msg = title + " was downloaded!"
-            print(msg)
-            return title
+        msg = title + " was downloaded!"
+        print(msg)
+        return title
 
 
 with open(LINKS_FILE,'r',encoding="utf8") as linksFile, open(PROCESSED_FILE,'w',encoding="utf8") as processedFile:
